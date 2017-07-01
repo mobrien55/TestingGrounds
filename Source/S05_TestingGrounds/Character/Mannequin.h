5d0c793997f183cff55299cc52d4a11e289de756
MATT-PC\Matt <mobrien55@hotmail.com>
2017-06-30 20:37:57 -0600

    Fixed Gun

--- Source/S05_TestingGrounds/Character/Mannequin.h
+++ Source/S05_TestingGrounds/Character/Mannequin.h
@@ -25,11 +25,13 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	virtual void UnPossessed() override;
+
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	TSubclassOf<class AGun> GunBlueprint;
 
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	void Fire();
+	void PullTrigger();
 
 private:
 	/** Pawn mesh: 1st person view (arms; seen only by self) */
